8e2bf0cfcbb2c392f1b28ae59734da789cf5f73f
Ben-The-Winner <80679675+Ben-The-Winner@users.noreply.github.com>
2024-05-26 01:14:23 +0300

    Update Graph.cpp
    the previous one was a mistake, it was an old version

--- Graph.cpp
+++ Graph.cpp
@@ -2,18 +2,18 @@
 using namespace std;
 #include "Graph.hpp"
 
-void Graph::loadGraph(int* matrix, int size) {
-  adjMatrix = new int[size*size];
-  matrixSize = size;
-  for (int i = 0; i < size*size; ++i) {
-    adjMatrix[i] = matrix[i];
+void Graph::loadGraph(int* matrix) {
+  adjMatrix = new int[matrix.size()][matrix.size()];
+  for (int i = 0; i < matrix.size(); ++i) {
+    for (int j = 0; j < matrix.size(); ++j) {
+        adjMatrix[i][j] = matrix[i][j];
   }
 }
 
 void Graph::printGraph() {
-  for(int i = 0; i < matrixSize; i++) {
-    for(int j = 0; j < matrixSize; j++) {
-      cout << adjMatrix[i * matrixSize + j] << " \n";
+  for(int i = 0; i < adjMatrix.size(); i++) {
+    for(int j = 0; j < adjMatrix.size(); j++) {
+      cout << adjMatrix[i][j] << endl;
     }
   }
 }
