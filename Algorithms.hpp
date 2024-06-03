c644dcc90dd378347000773600f60d620250be50
Ben-The-Winner <80679675+Ben-The-Winner@users.noreply.github.com>
2024-06-03 17:40:46 +0300

    Update Algorithms.hpp
    its running version

--- Algorithms.hpp
+++ Algorithms.hpp
@@ -2,12 +2,15 @@
 #define ALGORITHMS_H
 using namespace std;
 #include <iostream>
+#include <stack>
 #include <vector>
 #include "Graph.hpp" 
 
 bool isConnected(Graph);
 
 vector<int> shortestPath(Graph, int, int);
+
+bool dfs(Graph, vector<bool>, int, vector<int>, vector<int>);
 
 vector<int> isContainsCycle(Graph);
 
